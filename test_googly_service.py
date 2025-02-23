e17ec3251e008c2ca79367521286b6ac1a3ec460
Carol A. <carol.jhasmin.azparrent@estudiantat.upc.edu>
2025-02-24 00:02:37 +0100

    test: add test for images without any eyes

--- test_googly_service.py
+++ test_googly_service.py
@@ -10,6 +10,7 @@ INPUT_IMAGE_PNG_PATH = "test_images/input.png"
 INPUT_IMAGE_JPG_PATH = "test_images/input.jpg"
 INPUT_PDF_PATH = "test_images/input.pdf"
 INPUT_IMAGE_GIF_PATH = "test_images/input.gif"
+INPUT_IMAGE_NO_EYES_PATH = "test_images/input_no_eyes.png"
 
 # Output directory
 OUTPUT_DIR = "test_images/output_images"
@@ -39,8 +40,6 @@ def test_png_to_png(client):  # test1
 
         assert response.status_code == 200
         assert response.mimetype == "image/png"  # The response should be PNG (header)
-        # assert response.headers["Content-Type"] == "image/png"
-
         assert response.data  # if there any content in the body? True; no response: empty-> False
 
         # Save the processed image
@@ -60,8 +59,6 @@ def test_jpg_to_png(client):  # test2
 
         assert response.status_code == 200
         assert response.mimetype == "image/png"  # The response should be PNG (header)
-        # assert response.headers["Content-Type"] == "image/jpg"
-
         assert response.data  # if there any content in the body? True; no response: empty-> False
 
          # Save processed image
@@ -72,7 +69,25 @@ def test_jpg_to_png(client):  # test2
         assert processed_img.format == "PNG"    # Check file format
 
 
-def test_pdf_rip(client):  # test3
+def test_no_eyes(client):  # test1.1
+    """ Test upload a valid PNG image as input and serve the processed PNG as output."""
+
+    with open(INPUT_IMAGE_NO_EYES_PATH, "rb") as img_file:
+        data = {"image": (img_file, "input.png")}
+        response = client.post("/googly_eyes", content_type="multipart/form-data", data=data)
+
+        assert response.status_code == 200
+        assert response.mimetype == "image/png"  # The response should be PNG (header)
+        assert response.data  # if there any content in the body? True; no response: empty-> False
+
+        # Save the processed image
+        output_path = save_output_image(response.data, "output_no_eyes.png")
+
+        # Verify output is a PNG
+        processed_img = Image.open(output_path)
+        assert processed_img.format == "PNG"    # Check file format
+
+def test_pdf_error(client):  # test3
     """ Test upload an invalid PDF document as input."""
 
     with open(INPUT_PDF_PATH, "rb") as img_file:
@@ -82,7 +97,7 @@ def test_pdf_rip(client):  # test3
         assert response.status_code == 400
 
 
-def test_gif_rip(client):  # test5
+def test_gif_error(client):  # test5
     """ Test upload an invalid GIF image as input."""
 
     with open(INPUT_IMAGE_GIF_PATH, "rb") as img_file:
@@ -92,7 +107,7 @@ def test_gif_rip(client):  # test5
         assert response.status_code == 400
 
 
-def test_get_rip(client):  # test4
+def test_get_error(client):  # test4
     """Test that a GET request to the endpoint returns 405 Method Not Allowed."""
 
     response = client.get("/googly_eyes")
